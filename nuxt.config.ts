8ce4cbf613e4ef2c9cf5519d3fcebc4384d6ca64
pmb2020 <pmb2020@163.com>
2022-11-18 15:50:01 +0800

    tailwindcss.screens

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	modules: ['@nuxtjs/tailwindcss'],
-	tailwindcss: {
-
-	},
 	app: {
 		head: {
 			charset: 'utf-8',
@@ -12,6 +9,28 @@ export default defineNuxtConfig({
 				{ name: 'viewport', content: 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' },
 				{ name: 'description', content: 'nuxt3项目实战之alone-blog：nuxt3+tailwindcss' }
 			]
+		}
+	},
+	tailwindcss: {
+		config: {
+			theme: {
+				screens: {
+					'sm': '640px',
+					// => @media (min-width: 640px) { ... }
+	
+					'md': '768px',
+					// => @media (min-width: 768px) { ... }
+	
+					'lg': '1024px',
+					// => @media (min-width: 1024px) { ... }
+	
+					'xl': '1280px',
+					// => @media (min-width: 1280px) { ... }
+	
+					'2xl': '1536px',
+					// => @media (min-width: 1536px) { ... }
+				}
+			}
 		}
 	},
 	css: [
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
 			<Meta name="description" :content="title" />
 			<Style type="text/css" children="body { background-color: green; }" />
 		</Head>
-		<p class="text-3xl text-green-500 m-20">这是nuxt3+tailwindcss3新项目</p>
+		<p class="text-3xl md:text-green-500 m-20">这是nuxt3+tailwindcss3新项目</p>
 		<nuxt-link to="/about">关于我们</nuxt-link><br>
 		<nuxt-link to="/info/2">进入详情页</nuxt-link>
 	</div>
