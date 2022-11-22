f79a96940b0c6cec18ffcb948db040b6f278934a
pmb2020 <pmb2020@163.com>
2022-11-22 09:39:01 +0800

    footer-vaa

--- components/alone/Footer.vue
+++ components/alone/Footer.vue
@@ -1,11 +1,11 @@
 <template>
 	<footer class="footer">
 		<p>
-			<nuxt-link class="" target="_blank" to="http://beian.miit.gov.cn" ref="nofollow">京ICP备202211220号-1 </nuxt-link>
+			<nuxt-link class="" target="_blank" to="http://beian.miit.gov.cn" ref="nofollow">京ICP备2022025170号-1 </nuxt-link>
 			<img style="display: inline-block;margin-left: 4px;" src="~/assets/images/gongan.jpeg" alt="">
-			<nuxt-link class="" target="_blank" to="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=1" ref="nofollow">京公网安备 115248536441号</nuxt-link>
+			<nuxt-link class="" target="_blank" to="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=1" ref="nofollow">京公网安备 11010802021733号</nuxt-link>
 		</p>
-		<p>某某科技有限公司 © 版权所有  公司地址：中国北京市海淀区中关村东路大西瓜科技打算302室</p>
+		<p>先锋音讯公司 © 版权所有  公司地址：中国北京市海淀区中关村东路66号世纪科贸大厦C座22层</p>
 	</footer>
 </template>
 
